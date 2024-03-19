97c312240b3c468990fb2c29c462004c6fe622f8
iMaxZorky <mkoprav@gmail.com>
2024-03-19 11:15:03 +0600

    feat: getIdGeneratorFunction done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -204,8 +204,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let number = startFrom;
+  return () => {
+    number += 1;
+    return number - 1;
+  };
 }
 
 module.exports = {
