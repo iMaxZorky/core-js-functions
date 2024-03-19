5fb661f46530368fc1e28ac8545807da11c28262
iMaxZorky <mkoprav@gmail.com>
2024-03-19 09:57:18 +0600

    feat: memoize done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -107,8 +107,9 @@ function getPolynom(...value) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const memFunc = func();
+  return () => memFunc;
 }
 
 /**
