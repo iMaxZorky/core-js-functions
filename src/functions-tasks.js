4221ee821a4c3d6e86d905fb4de5d7ea380e2cc8
iMaxZorky <mkoprav@gmail.com>
2024-03-19 10:06:37 +0600

    feat: retry done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -127,8 +127,14 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    try {
+      return func();
+    } catch (error) {
+      return attempts ? retry(func, attempts)() : attempts;
+    }
+  };
 }
 
 /**
